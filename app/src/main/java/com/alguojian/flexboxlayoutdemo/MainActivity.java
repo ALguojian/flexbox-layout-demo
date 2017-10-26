0c2da2f795dc27d5ec2c6983a737d095bfe291e8
ALguojian <3324883037@qq.com>
2017-10-26 11:05:43 +0800

    添加监听

--- app/src/main/java/com/alguojian/flexboxlayoutdemo/MainActivity.java
+++ app/src/main/java/com/alguojian/flexboxlayoutdemo/MainActivity.java
@@ -6,15 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.flexbox.FlexboxLayout;
 
-import static com.google.android.flexbox.AlignSelf.FLEX_END;
 import static com.google.android.flexbox.AlignSelf.FLEX_START;
 import static com.google.android.flexbox.FlexDirection.ROW;
-import static com.google.android.flexbox.FlexWrap.WRAP_REVERSE;
+import static com.google.android.flexbox.FlexWrap.WRAP;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,37 +42,54 @@ public class MainActivity extends AppCompatActivity {
         flexBoxLayout.setFlexDirection(ROW);
 
         //设置换行
-        flexBoxLayout.setFlexWrap(WRAP_REVERSE);
+        flexBoxLayout.setFlexWrap(WRAP);
 
         //设置元素主轴方向上的对齐方式
-        flexBoxLayout.setJustifyContent(FLEX_END);
+        flexBoxLayout.setJustifyContent(FLEX_START);
 
         //设置水平方向分割线
         flexBoxLayout.setShowDividerHorizontal(DividerItemDecoration.VERTICAL);
 
         for (int i = 0; i < 15; i++) {
-            textView = new TextView(this);
-            textView.setText(i + "just贝贝" + (i * i));
-            textView.setGravity(Gravity.CENTER);
-            textView.setPadding(20, 5, 20, 5);
-            textView.setTextColor(Color.BLACK);
-            textView.setBackgroundColor(Color.YELLOW);
-            flexBoxLayout.addView(textView);
-        }
-        ViewGroup.LayoutParams params = textView.getLayoutParams();
 
-        if (params instanceof FlexboxLayout.LayoutParams) {
-            FlexboxLayout.LayoutParams layoutParams = (FlexboxLayout.LayoutParams) params;
-            //子view的属性
-            layoutParams.setFlexBasisPercent(0.5f);
-            layoutParams.setAlignSelf(FLEX_START);
-            layoutParams.setMinHeight(dpToPx(20));
-            layoutParams.setMinWidth(dpToPx(40));
+            flexBoxLayout.addView(getItemView("just贝贝h喀"));
         }
+
+    }
+
+    private TextView getItemView(final String string) {
+
+        textView = new TextView(this);
+        textView.setText(string);
+        textView.setGravity(Gravity.CENTER);
+        textView.setPadding(13, 5, 13, 5);
+        textView.setTextColor(Color.BLUE);
+        textView.setBackgroundColor(Color.YELLOW);
+        //设置点击事件
+        textView.setClickable(true);
+
+        //设置外间距
+        FlexboxLayout.LayoutParams params1 = new FlexboxLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params1.setMargins(dpToPx(14),dpToPx(6),dpToPx(6),dpToPx(14));
+        textView.setLayoutParams(params1);
+
+        //添加点击事件
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return textView;
+
     }
 
     /**
      * px转dp工具类
+     *
      * @param pixel
      * @return
      */
@@ -82,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * dp转px工具类
+     *
      * @param dp
      * @return
      */
